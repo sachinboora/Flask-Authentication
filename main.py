ba9b95430e5f8461ae202f211b5f545ec563b79b
Sachin <sachnboora@gmail.com>
2022-07-03 15:03:40 +0530

    Authenticating Users

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 # CREATE TABLE IN DB
 class User(UserMixin, db.Model):
@@ -34,31 +42,49 @@ def register():
         new_user = User(
             email=request.form.get('email'),
             name=request.form.get('name'),
-            password = generate_password_hash(password = request.form.get('password'), method='sha256', salt_length=8)
+            password=generate_password_hash(password=request.form.get('password'), method='sha256', salt_length=8)
         )
         db.session.add(new_user)
         db.session.commit()
+
+        # Login user after registration
+        login_user(new_user)
+
         return redirect(url_for('secrets', name=new_user.name))
 
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()  # Get user from DB
+
+        # Check password against hashed password stored in DB
+        if check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('secrets', name=user.name))
+
     return render_template("login.html")
 
 
 @app.route('/secrets/<name>')
+@login_required
 def secrets(name):
     return render_template("secrets.html", name=name)
 
 
 @app.route('/logout')
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route('/download')
+@login_required
 def download():
     return send_from_directory('static', path='files/cheat_sheet.pdf')
 
